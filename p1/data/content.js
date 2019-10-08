a5d421c429e3eba18093aedd668b733a442415de
Grace Punzalan <gpunzalan18@gmail.com>
2019-10-07 23:49:17 -0400

    Cleaned up code

--- p1/data/content.js
+++ p1/data/content.js
@@ -1,28 +1,28 @@
 let content = {
-  title: "Project 1 - Mystery Game in Vue.js",
-  instruction: "Pick a category:",
-  categories: [
-    {
-      id: 'fruits',
-      list: ['apple', 'avocado', 'watermelon', 'blackberry', 'mango']
+    title: "Project 1 - Mystery Game in Vue.js",
+    instruction: "Pick a category:",
+    submitLabel: "Guess a letter or solve the puzzle:",
+    categories: [
+        {
+            id: 'fruits',
+            list: ['apple', 'avocado', 'watermelon', 'blackberry', 'mango']
+        },
+        {
+            id: 'animals',
+            list: ['gorilla', 'snake', 'bat', 'crocodile', 'elephant']
+        },
+        {
+            id: 'color',
+            list: ['red', 'blue', 'yellow', 'white', 'orange']
+        }
+    ],
+    buttons: {
+        generate: "Generate Puzzle",
+        submit: "Submit",
     },
-    {
-      id: 'animals',
-      list: ['gorilla', 'snake', 'bat', 'crocodile', 'elephant']
-    },
-    {
-      id: 'color',
-      list: ['red', 'blue', 'yellow', 'white', 'orange']
+    messages: {
+        win: "Congratulations, you solved the puzzle!",
+        lose: "You\'ve entered 6 invalid letters. Better luck next time!",
+        incorrect: "Your answer is incorrect, try again!"
     }
-  ],
-  buttons: {
-    generate: "Generate Puzzle",
-    guess: "Guess a Letter",
-    solve: "Solve the Puzzle"
-  },
-  messages: {
-    win: "Congratulations, you solved the puzzle!",
-    lose: "You\'ve entered 6 invalid letters. Better luck next time!",
-    incorrect: "Your answer is incorrect, try again!"
-  }
 }
--- p1/scripts.js
+++ p1/scripts.js
@@ -1,93 +1,94 @@
 let app = new Vue({
-  el: '#app',
-  data: {
-    content: content,
-    showSecondView: false,
-    randomWord: '',
-    picked: '',
-    puzzle: '',
-    guessedLetter: '',
-    guesses: [],
-    invalidAttempt: 0,
-    userMsg: '',
-    inputView: true,
-    msgColor: '',
-    guessBtn: '',
-    btnName: ''
-  },
-  methods: {
-    generatePuzzle() {
-      this.reset();
-      this.showSecondView = true;
-      categoryList = this.content.categories[this.picked].list;
-      this.randomWord = categoryList[Math.floor(Math.random() * 5)];
+    el: '#app',
+    data: {
+        content: content,
+        secondView: false,
+        inputView: true,
+        invalidAttempt: 0,
+        guesses: [],
+        randomWord: '',
+        pickedCategory: '',
+        puzzle: '',
+        userInput: '',
+        userMsg: '',
+        msgColor: '',
+        guessBtn: '',
+    },
+    methods: {
+        generatePuzzle() {
+            this.reset();
+            let categoryList = this.content.categories[this.pickedCategory].list;
+            this.randomWord = categoryList[Math.floor(Math.random() * 5)];
 
-      for (let i = 0; i < this.randomWord.length; i++) {
-        this.puzzle += "_ ";
-      }
-    },
-    guessEnter() {
-      if (this.guessedLetter.length == 1) {
-        this.guessLetter();
-      } else if (this.guessedLetter.length > 1) {
-        this.solvePuzzle();
-      }
-    },
-    guessLetter() {
-      let foundIndex = [];
-      let display = '';
-      if (this.guessedLetter.length === 1) {
-        this.userMsg = '';
-        for (let i = 0; i < this.randomWord.length; i++) {
-          if (this.guessedLetter == this.randomWord.toLowerCase()[i]) {
-            display += this.guessedLetter + ' ';
-            foundIndex.push(i);
-          } else {
-            display += this.puzzle[i * 2] + ' ';
-          }
-        }
-        this.puzzle = display;
+            for (let i = 0; i < this.randomWord.length; i++) {
+                this.puzzle += "_ ";
+            }
+        },
+        guess() {
+            if (this.userInput.length == 1) {
+                this.guessLetter();
+            } else if (this.userInput.length > 1) {
+                this.solvePuzzle();
+            } else {
+                this.msgColor = 'red';
+                this.userMsg = "Enter a value."
+            }
+        },
+        guessLetter() {
+            if (this.userInput.length === 1) {
+                let foundIndex = [];
+                let display = '';
+                this.userMsg = '';
+                for (let i = 0; i < this.randomWord.length; i++) {
+                    if (this.userInput.toLowerCase() == this.randomWord[i]) {
+                        display += this.userInput.toLowerCase() + ' ';
+                        foundIndex.push(i);
+                    } else {
+                        display += this.puzzle[i * 2] + ' ';
+                    }
+                }
+                this.puzzle = display;
 
-        if (foundIndex.length === 0) {
-          this.guesses.push(this.guessedLetter);
-          this.invalidAttempt += 1;
-        }
-        this.isWinner();
-        this.guessedLetter = '';
-      }
-    },
-    solvePuzzle() {
-      if (this.guessedLetter.length >= 1 && this.guessedLetter == this.randomWord.toLowerCase()) {
-        this.msgColor = 'green';
-        this.userMsg = this.content.messages.win;
-        this.inputView = false;
-        this.guesses = [];
-        this.puzzle = this.guessedLetter;
-      } else {
-        this.msgColor = 'red';
-        this.userMsg = this.content.messages.incorrect;
-      }
-      this.guessedLetter = '';
-    },
-    reset() {
-      this.puzzle = '';
-      this.userMsg = '';
-      this.guesses = [];
-      this.inputView = true;
-      this.guessedLetter = '';
-      this.invalidAttempt = 0;
-    },
-    isWinner() {
-      if (!this.puzzle.includes('_')) {
-        this.msgColor = 'green';
-        this.userMsg = this.content.messages.win;
-        this.inputView = false;
-      } else if (this.invalidAttempt >= 6) {
-        this.msgColor = 'red';
-        this.userMsg = this.content.messages.lose;
-        this.puzzle = this.randomWord;
-        this.inputView = false;
-      }
-    },
-  }
+                if (foundIndex.length === 0) {
+                    this.guesses.push(this.userInput.toLowerCase());
+                    this.invalidAttempt += 1;
+                }
+                this.isWinner();
+                this.userInput = '';
+            }
+        },
+        solvePuzzle() {
+            if (this.userInput.length >= 1 && this.userInput.toLowerCase() == this.randomWord) {
+                this.inputView = false;
+                this.puzzle = this.userInput.toLowerCase();
+                this.msgColor = 'green';
+                this.userMsg = this.content.messages.win;
+            } else {
+                this.msgColor = 'red';
+                this.userMsg = this.content.messages.incorrect;
+            }
+            this.userInput = '';
+        },
+        reset() {
+            this.puzzle = '';
+            this.userMsg = '';
+            this.userInput = '';
+            this.guesses = [];
+            this.inputView = true;
+            this.secondView = true;
+            this.invalidAttempt = 0;
+        },
+        isWinner() {
+            if (!this.puzzle.includes('_')) {
+                this.msgColor = 'green';
+                this.userMsg = this.content.messages.win;
+                this.inputView = false;
+            } else if (this.invalidAttempt >= 6) {
+                this.msgColor = 'red';
+                this.userMsg = this.content.messages.lose;
+                this.puzzle = this.randomWord;
+                this.inputView = false;
+            }
+        },
+    }
 })
