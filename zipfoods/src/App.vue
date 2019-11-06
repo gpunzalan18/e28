6fe13ed2e464af77fcf3989851e341b17cc916e4
Grace Punzalan <gpunzalan18@gmail.com>
2019-11-06 17:03:00 -0500

    Progress on Zipfoods after Week 9 Part 3

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -2,13 +2,21 @@
   <div id="app">
     <img id="logo" alt="Vue logo" src="./assets/images/zipfoods-logo.png" />
     <p>ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.</p>
+    <show-featured category="frozen"></show-featured>
+    <show-products></show-products>
   </div>
 </template>
 
 <script>
+import ShowProducts from "./components/ShowProducts";
+import ShowFeatured from "./components/ShowFeatured";
+
 export default {
   name: "app",
-  components: {}
+  components: {
+    ShowProducts,
+    ShowFeatured
+  }
 };
 </script>
 
---
+++ zipfoods/src/components/ShowProducts.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="products">
+    <h2>Products</h2>
+    <show-product v-for="product in products" :product="product" :key="product.id"></show-product>
+  </div>
+</template>
+
+
+<script>
+import ShowProduct from "./ShowProduct";
+import { products } from "./../products";
+
+export default {
+  name: "ShowProducts",
+  components: {
+    ShowProduct
+  },
+  data: function() {
+    return {
+      products: products
+    };
+  }
+};
+</script>
+
+
+<style scoped>
+</style>
